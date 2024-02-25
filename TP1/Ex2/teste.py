37cfd52ac214bdb1ad2d80ab7edf83ecc04a5f0c
simaobarroso <simao.barroso@outlook.pt>
2024-02-25 23:35:48 +0000

    Ligeiramente mais comentada

---
+++ TP1/Ex2/teste.py
@@ -0,0 +1,21 @@
+import os
+
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+
+data = b"olo oasodaosasdasdasdasdasdasdas"
+
+aad = b"authenticated but unencrypted data"
+
+key = ChaCha20Poly1305.generate_key()
+
+chacha = ChaCha20Poly1305(key)
+
+nonce = os.urandom(12)
+
+ct = chacha.encrypt(nonce, data, aad)
+
+ct2 = chacha.encrypt(nonce, ct, aad)
+
+print(ct2)
+
+chacha.decrypt(nonce, ct, aad)
